63f01e6f594e37a1d43657fe5e02f272e7265c86
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-06-19 21:27:39 +0300

    Custom color theme, Assets, Extensions

---
+++ CryptoBankingApp/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  CryptoBankingApp
+//
+//  Created by Huseyn Abishov on 19.06.2022.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
