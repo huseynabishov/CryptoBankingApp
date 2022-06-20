c8f1e853bccb69aa3c519ea6b0f1195b800ea4d3
huseynabishov <92877169+huseynabishov@users.noreply.github.com>
2022-06-20 21:20:17 +0300

    Update

---
+++ CryptoBankingApp/Core/Components/CircleButton/CircleButtonAnimationView.swift
@@ -0,0 +1,32 @@
+//
+//  CircelButtonAnimationView.swift
+//  CryptoBankingApp
+//
+//  Created by Huseyn Abishov on 19.06.2022.
+//
+
+import SwiftUI
+
+struct CircleButtonAnimationView: View {
+    
+    @Binding  var animate: Bool
+    
+    var body: some View {
+        Circle()
+            .stroke(lineWidth: 5.0)
+            .scale(animate ? 1.0 : 0.0)
+            .opacity(animate ? 0.0 : 1.0)
+            .animation(animate ? Animation.easeOut(duration: 1.0) : .none)
+            .onAppear {
+                animate.toggle()
+            }
+    }
+}
+
+struct CircleButtonAnimationView_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleButtonAnimationView(animate: .constant(false))
+            .foregroundColor(.red)
+            .frame(width: 100, height: 100)
+    }
+}
---
+++ CryptoBankingApp/Models/CoinModel.swift
@@ -0,0 +1,15 @@
+//
+//  CoinModel.swift
+//  CryptoBankingApp
+//
+//  Created by Huseyn Abishov on 19.06.2022.
+//
+
+import Foundation
+
+
+class CoinModel {
+    
+    
+    
+}
